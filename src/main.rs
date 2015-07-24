c7d5debae308b8ace7af7ce8015c1a2ff7239ec8
Joshua Miller <jshuasmiller@gmail.com>
2015-07-24 16:49:48 -0500

    update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate nalgebra as na;
+use na::{Vec3, Rot3, Rotation};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let     a = Vec3::new(1.0f64, 1.0, 1.0);
+    let mut b = Rot3::new(na::zero());
+
+    b.append_rotation_mut(&a);
+
+    assert!(na::approx_eq(&na::rotation(&b), &a));
+    println!("{}", a.x);
+
 }
