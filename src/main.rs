ab075e2ec4f1ab4f550e4b4c1e967ff3a6d11cb6
Joshua Miller <jshuasmiller@gmail.com>
2015-07-25 13:11:05 -0500

    use tuple structs

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,7 @@ const BOX_Y: f64 = 300.0;
 const BOX_Z: f64 = 300.0;
 const SCREEN: f64 = 600.0;
 
-struct Vec3d {
-    x: f64,
-    y: f64,
-    z: f64,
-}
+struct Vec3d (f64, f64, f64);
 
 pub struct Particle {
     r: Vec3d,
@@ -33,20 +29,6 @@ pub struct Particle {
     m: f64,
     rad: f64,
     ellipse: Ellipse,
-}
-
-impl Add for Vec3d {
-    type Output = Vec3d;
-    fn add(self, other: Vec3d) -> Vec3d {
-        Vec3d {x: self.x + other.x, y: self.y + other.y, z: self.z + other.z}
-    }
-}
-
-impl Sub for Vec3d {
-    type Output = Vec3d;
-    fn sub(self, other: Vec3d) -> Vec3d {
-        Vec3d {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
-    }
 }
 
 // fn force_gravity (&mut p: Particle) {
@@ -63,10 +45,10 @@ impl Particle {
            rx: f64, ry: f64, rz: f64,
            vx: f64, vy: f64, vz: f64) -> Particle{
         Particle {
-            r: Vec3d {x: rx, y: ry, z: rz},
-            v: Vec3d {x: vx, y: vy, z: vz},
-            a: Vec3d {x: 0.0, y: 0.0, z: 0.0},
-            a2: Vec3d {x: 0.0, y: 0.0, z: 0.0},
+            r: Vec3d (rx, ry, rz),
+            v: Vec3d (vx, vy, vz),
+            a: Vec3d (0.0, 0.0, 0.0),
+            a2: Vec3d (0.0, 0.0, 0.0),
             m: 1.0,
             rad: rad,
             ellipse: Ellipse::new([0.5, 0.5, 0.5, 1.0]),
@@ -74,50 +56,50 @@ impl Particle {
     }
 
     fn dist(&self) -> f64 {
-        (self.r.x*self.r.x + self.r.y*self.r.y + self.r.z*self.r.z).sqrt()
+        (self.r.0*self.r.0 + self.r.1*self.r.1 + self.r.2*self.r.2).sqrt()
     }
 
     fn projected_radius(&self) -> f64 {
         let fov = FOVY / 2.0 * PI / 180.0;
-        let d = self.r.z + SCREEN;
+        let d = self.r.2 + SCREEN;
         1.0 / fov.tan() * self.rad / (d*d - self.rad*self.rad).sqrt()
     }
 
     fn projected(&self, r: f64) -> f64 {
-        SCREEN*r/(self.r.z + SCREEN*2.0)
+        SCREEN*r/(self.r.2 + SCREEN*2.0)
     }
 
     fn verlet(&mut self, dt: f64){
-        self.r.x = self.r.x + self.v.x*dt + 0.5*self.a.x*dt*dt;
-        self.r.y = self.r.y + self.v.y*dt + 0.5*self.a.y*dt*dt;
-        self.r.z = self.r.z + self.v.z*dt + 0.5*self.a.z*dt*dt;
+        self.r.0 = self.r.0 + self.v.0*dt + 0.5*self.a.0*dt*dt;
+        self.r.1 = self.r.1 + self.v.1*dt + 0.5*self.a.1*dt*dt;
+        self.r.2 = self.r.2 + self.v.2*dt + 0.5*self.a.2*dt*dt;
 
     }
 
     fn update(&mut self, dt: f64) {
-        if self.r.x < -BOX_X  {
-            self.v.x = -1.0 * self.v.x;
-            self.r.x = -BOX_X * 0.99;
+        if self.r.0 < -BOX_X  {
+            self.v.0 = -1.0 * self.v.0;
+            self.r.0 = -BOX_X * 0.99;
         }
-        if self.r.x > BOX_X  {
-            self.v.x = -1.0 * self.v.x;
-            self.r.x = BOX_X * 0.99;
+        if self.r.0 > BOX_X  {
+            self.v.0 = -1.0 * self.v.0;
+            self.r.0 = BOX_X * 0.99;
         }
-        if self.r.y < -BOX_Y  {
-            self.v.y = -1.0 * self.v.y;
-            self.r.y = -BOX_Y * 0.99;
+        if self.r.1 < -BOX_Y  {
+            self.v.1 = -1.0 * self.v.1;
+            self.r.1 = -BOX_Y * 0.99;
         }
-        if self.r.y > BOX_Y  {
-            self.v.y = -1.0 * self.v.y;
-            self.r.y = BOX_Y * 0.99;
+        if self.r.1 > BOX_Y  {
+            self.v.1 = -1.0 * self.v.1;
+            self.r.1 = BOX_Y * 0.99;
         }
-        if self.r.z < -BOX_Z  {
-            self.v.z = -1.0 * self.v.z;
-            self.r.z = -BOX_Z * 0.99;
+        if self.r.2 < -BOX_Z  {
+            self.v.2 = -1.0 * self.v.2;
+            self.r.2 = -BOX_Z * 0.99;
         }
-        if self.r.z > BOX_Z  {
-            self.v.z = -1.0 * self.v.z;
-            self.r.z = BOX_Z * 0.99;
+        if self.r.2 > BOX_Z  {
+            self.v.2 = -1.0 * self.v.2;
+            self.r.2 = BOX_Z * 0.99;
         }
         // self.verlet(dt);
     }
@@ -144,8 +126,8 @@ impl App {
 
                 let mut draw = |p: &Particle| {
                     let rad = p.projected_radius() * (args.height as f64)*0.5;
-                    let xx = p.projected(p.r.x);
-                    let yy = p.projected(p.r.y);
+                    let xx = p.projected(p.r.0);
+                    let yy = p.projected(p.r.1);
                     p.ellipse.draw(
                         [x+xx-rad*0.5, y-(yy-rad*0.5), rad, rad],
                         &c.draw_state, c.transform, gl);
@@ -163,9 +145,9 @@ impl App {
 
     fn update(&mut self, args: &UpdateArgs) {
         for i in 0..self.particles.len(){
-            self.particles[i].r.x += self.particles[i].v.x * args.dt;
-            self.particles[i].r.y += self.particles[i].v.y * args.dt;
-            self.particles[i].r.z += self.particles[i].v.z * args.dt;
+            self.particles[i].r.0 += self.particles[i].v.0 * args.dt;
+            self.particles[i].r.1 += self.particles[i].v.1 * args.dt;
+            self.particles[i].r.2 += self.particles[i].v.2 * args.dt;
             self.particles[i].update(args.dt);
             // let lum = ((PI/2.0 - (self.particles[i].dist()/200.).atan())
             //     /(PI/2.0)) as f32;
