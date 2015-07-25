293bc9c483295daf9cff6a7ace2afdbdff835b34
Joshua Miller <jshuasmiller@gmail.com>
2015-07-24 22:51:26 -0500

    window with vector of particle structs

--- src/main.rs
+++ src/main.rs
@@ -23,40 +23,53 @@ use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
-const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-const RED:   [f32; 4] = [0.3, 0.3, 0.3, 1.0];
+const BACKGROUND: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-pub struct Square {
+struct Vec3d {
     x: f64,
     y: f64,
-    rot: f64,
+    z: f64,
+}
+
+pub struct Particle {
+    p: Vec3d,
+    v: Vec3d
 }
 
 
 pub struct App {
     gl: GlGraphics,
-    square: Square,
+    particles: Vec<Particle>,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let x = (args.width / 3) as f64 + self.square.x;
-        let y = (args.height / 2) as f64 + self.square.y;
+        let x = (args.width / 3) as f64;
+        let y = (args.height / 2) as f64;
+        // let x = (args.width / 3) as f64 + self.square.x;
+        // let y = (args.height / 2) as f64 + self.square.y;
+
 
         self.gl.draw(
             args.viewport(), |c, gl| {
-                clear(BLACK, gl);
-                let transform = c.transform.trans(x, y);
-                rectangle(RED, square, transform, gl);
+                clear(BACKGROUND, gl);
+
+                let transform = c.transform.trans(100.0, 100.0);
+
+                Ellipse::new([0.5, 0.5, 0.5, 1.0])
+                    .draw([x, y, 20.0, 20.0],
+                          &c.draw_state,
+                          c.transform,
+                          gl);
+
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        self.square.x += 10.0 * args.dt;
-        self.square.y += 10.0 * args.dt;
+        // self.square.x += 10.0 * args.dt;
+        // self.square.y += 10.0 * args.dt;
     }
 
 }
@@ -64,26 +77,22 @@ impl App {
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    // Create an Glutin window.
     let window = Window::new(
         WindowSettings::new(
-            "spinning-square", [200, 200])
+            "spinning-square", [1200, 800])
             .opengl(opengl)
             .exit_on_esc(true));
 
-    // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        square: Square{x: 0.0, y: 0.0, rot: 0.0}
+        particles: vec![Particle {
+            p: Vec3d {x: 0.0, y:0.0, z:0.0},
+            v: Vec3d {x: 0.0, y:0.0, z:0.0},
+        }]
     };
 
     for e in window.events() {
-        if let Some(r) = e.render_args() {
-            app.render(&r);
-        }
-
-        if let Some(u) = e.update_args() {
-            app.update(&u);
-        }
+        if let Some(r) = e.render_args() {app.render(&r);}
+        if let Some(u) = e.update_args() {app.update(&u);}
     }
 }
