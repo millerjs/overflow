6f8d66d6b91faba33ff052f791ef0256299be5ca
Joshua Miller <jshuasmiller@gmail.com>
2015-07-26 11:53:41 -0500

    fixed lj potential

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,22 @@ extern crate num;
 extern crate rand;
 extern crate image;
 
-use std::fs::File;
-use std::path::Path;
 use std::f64::consts::PI;
-use std::cmp;
-use rand::Rng;
+use std::thread;
 use rand::distributions::{IndependentSample, Range};
 use image::{ImageBuffer, Rgb};
 
-const BACKGROUND: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const FOVY: f64 = 60.0;
+
 const BOX_X: f64 = 600.0;
 const BOX_Y: f64 = 300.0;
 const BOX_Z: f64 = 300.0;
 
-const IMAGE_X: i32 = 1200;
-const IMAGE_Y: i32 = 800;
+const IMAGE_X: i32 = 400;
+const IMAGE_Y: i32 = 300;
 
 const SCREEN: f64 = 600.0;
-const SCALE: f64 = 1.0;
+const SCALE: f64 = IMAGE_X as f64 / 1200.0;
 
 pub struct Particle {
     r: [f64; 3],      // location
@@ -38,6 +35,7 @@ pub struct Image {
     height: i32,
 }
 
+#[allow(dead_code)]
 impl Image {
     fn new(width: i32, height: i32) -> Image {
         Image {
@@ -72,13 +70,11 @@ impl Image {
     }
 
     fn draw_circle(&mut self, _x: f64, _y: f64, r: f64, pixel: [f32; 4]) {
-        println!("Circle at ({}, {}): {}", _x, _y, r);
-        let x0 = (self.width / 2 - self.translate(_x)) as i32;
+        let x0 = (self.width/2 - self.translate(_x)) as i32;
         let y0 = (self.height/2 - self.translate(_y)) as i32;
-        println!("Translated to at ({}, {}): {}", x0, y0, r);
         let mut x = r as i32;
         let mut y = 0;
-        let mut decisionOver2 = 1 - x;
+        let mut condition = 1 - x;
 
         while x > y {
             self.set_pixel(( x + x0) as i32, ( y + y0) as i32, pixel);
@@ -90,26 +86,26 @@ impl Image {
             self.set_pixel(( x + x0) as i32, (-y + y0) as i32, pixel);
             self.set_pixel(( y + x0) as i32, (-x + y0) as i32, pixel);
             y += 1;
-            if (decisionOver2<=0) {
-                // Change in decision criterion for y -> y+1
-                decisionOver2 += 2 * y + 1;
+            if condition <=0 {
+                condition += 2 * y + 1;
             } else {
                 x -= 1;
-                // Change for y -> y+1, x -> x-1
-                decisionOver2 += 2 * (y - x) + 1;
+                condition += 2 * (y - x) + 1;
             }
         }
     }
 
     fn clear(&mut self, color: [f32; 4]) {
         for i in 0..(self.width*self.height) as usize {
-            self.pixels[i] = [0; 4];
+            for j in 0..4 {
+                self.pixels[i][j] = (color[j]*255 as f32) as u8;
+            }
         }
     }
 
-    fn save(&mut self, path: &str) {
-        println!("Generating image");
-        let mut imgbuf = image::ImageBuffer::<Rgb<u8>>::new(
+    fn save(&mut self, step: u32) -> thread::JoinHandle<()> {
+        println!(" - Generating image at step {}", step);
+        let mut imgbuf = ImageBuffer::<Rgb<u8>>::new(
             self.width as u32, self.height as u32);
 
         // Iterate over the coordiantes and pixels of the image
@@ -121,15 +117,20 @@ impl Image {
             }
         }
 
-        println!("Saving image to '{}'", path);
-        imgbuf.save(path).unwrap();
+        thread::spawn(move || {
+            let path = format!("output/test_{:08}.png", step);
+            println!(" - Saving image to '{}'", path);
+            imgbuf.save(&*path).unwrap();
+            println!(" - Wrote '{}'", path);
+        })
+
     }
 
 }
 
 
 pub struct Domain {
-    step: i32,
+    render_step: u32,
     t: f64,
     dt: f64,
     particles: Vec<Particle>,
@@ -137,6 +138,7 @@ pub struct Domain {
     image: Image,
 }
 
+#[allow(dead_code)]
 fn v_add(a: [f64; 3], b: [f64; 3]) -> [f64; 3] {
     let mut c = [0.0; 3];
     for i in 0..3 {
@@ -161,7 +163,7 @@ fn v_dot(a: [f64; 3], b: [f64; 3]) -> f64 {
     c
 }
 
-
+#[allow(dead_code)]
 impl Particle {
 
     /// Create a new particle
@@ -198,7 +200,7 @@ impl Particle {
         let fov = FOVY / 2.0 * PI / 180.0;
         let d = self.r[2] + SCREEN;
         (1.0 / fov.tan() * self.rad /
-            (d*d - self.rad*self.rad).sqrt() * IMAGE_Y/2 as f64).abs()
+            (d*d - self.rad*self.rad).sqrt() * (IMAGE_Y/2) as f64).abs()
     }
 
     /// Get the x or y location as projected on the screen
@@ -211,6 +213,7 @@ impl Particle {
         for j in 0..3 {
             self.r[j] += self.v[j]*dt + 0.5*self.a[j]*dt*dt;
             self.v[j] += 0.5*(self.a[j] + self.ap[j])*dt;
+            self.v[j] = self.v[j].min(1e4).max(-1e4);
         }
     }
 
@@ -222,17 +225,18 @@ impl Particle {
     /// Add the acceleration from pseudo Lennard-Jones potentials from
     /// surrounding particles
     fn force_lj (&mut self, positions: &Vec<[f64; 3]>) {
-        let sigma = self.rad * 1.0e-5;
-        let eps = 100000.0;
+        let sigma = self.rad / 1.122 * 2.0;
+        let eps = 1.0e5;
         for p in positions.iter() {
             let r = v_sub(self.r, *p);
             let r_sq = v_dot(r, r);
-            if r_sq.abs() > 0.01 && r_sq.abs() < 5.0 {
+            if r_sq > 1e-5 {
+                let norm = r_sq.sqrt();
+                let temp_7 = (sigma / norm).powi(7);
+                let temp_13 = (sigma / norm).powi(13);
+                let a = eps / sigma * (2.0 * temp_13 - temp_7);
                 for j in 0..3 {
-                    let temp_6 = (sigma / self.r[j]).powi(6);
-                    let temp_12 = temp_6 * temp_6;
-                    self.a[j] = (eps * r[j] * (temp_12 - 0.5 * temp_6)/
-                                  (r_sq * self.m));
+                    self.a[j] -= a * r[0] / norm;
                 }
             }
         }
@@ -302,9 +306,33 @@ impl Domain {
 
     fn setup(&mut self) {
 
-        for _ in 0..100 {
-            self.particles.push(Particle::random_pos());
+        let n = 5;
+        let rad = 10.0;
+        let mut rng = rand::thread_rng();
+        let range = Range::new(-rad/3.0, rad/3.0);
+
+        for x in 0..n {
+            for y in 0..n {
+                for z in 0..n {
+                    self.particles.push(Particle::new(
+                        rad,
+                        x as f64 * rad * 5.0 + range.ind_sample(&mut rng),
+                        y as f64 * rad * 5.0 + range.ind_sample(&mut rng),
+                        z as f64 * rad * 5.0 + range.ind_sample(&mut rng),
+                        0.0, 0.0, 0.0));
+                }
+            }
         }
+
+        // for _ in 0..250 {
+        //     self.particles.push(Particle::random_pos());
+        // }
+
+        // self.particles.push(Particle::new(
+        //     10.0, -BOX_X/2.0, 0.0, 0.0, 0.0, 0.0, 0.0));
+        // self.particles.push(Particle::new(
+        //     10.0, BOX_X/2.0, 0.0,  0.0, 0.0, 0.0, 0.0));
+
 
         self.bbox.push(Particle::new(10.0, -BOX_X, -BOX_Y,  BOX_Z, 0.0, 0.0, 0.0));
         self.bbox.push(Particle::new(10.0,  BOX_X, -BOX_Y,  BOX_Z, 0.0, 0.0, 0.0));
@@ -318,14 +346,19 @@ impl Domain {
 
 
     fn render(&mut self) {
+        self.image.clear([0.0; 4]);
         for p in self.bbox.iter(){
             self.image.draw_circle(
                 p.projected(p.r[0]), p.projected(p.r[1]), p.projected_radius(),
-                [1.0; 4]);
+                [0.5, 0.5, 0.8, 1.0]);
         }
-        // for p in self.particles.iter_mut(){
+        for p in self.particles.iter(){
+            self.image.draw_circle(
+                p.projected(p.r[0]), p.projected(p.r[1]), p.projected_radius(),
+                [0.5; 4]);
 
-        // }
+        }
+        self.render_step += 1;
     }
 
     fn update(&mut self) {
@@ -336,6 +369,11 @@ impl Domain {
         for p in self.particles.iter_mut(){
             p.update(&positions, self.dt);
         }
+        self.t += self.dt;
+    }
+
+    fn print_state(&self) {
+        println!("Domain:\tt={:.*}", 2, self.t);
     }
 
 }
@@ -343,22 +381,31 @@ impl Domain {
 
 fn main() {
 
-    let max_iterations = 256u16;
-
     let mut domain = Domain {
         particles: vec![],
         bbox: vec![],
-        step: 0,
-        dt: 1.0,
+        render_step: 0,
+        dt: 1.0e-2,
         t: 0.0,
         image: Image::new(IMAGE_X, IMAGE_Y),
     };
 
     println!("Created image buffer of length {} KB", IMAGE_X*IMAGE_Y/1000);
-
     domain.setup();
-    domain.update();
-    domain.render();
-    domain.image.save("output/test.png");
+
+    let max_time = 3.0;
+    let mut step = 0;
+
+    while domain.t < max_time {
+        domain.update();
+        if step % (1.0/domain.dt/10.0) as i32 == 0 {
+            domain.render();
+            domain.image.save(domain.render_step);
+            domain.print_state();
+        }
+        step += 1;
+    }
+
+    thread::sleep_ms(1000);
 
 }
